b844387001349e93837a639938010dd539501f79
mallikharjun9018 <a1.mallikharjunareddy@gmail.com>
2020-02-14 21:19:17 +0530

    removed xpath

--- src/newMalli/Tessssst.java
+++ src/newMalli/Tessssst.java
@@ -3,49 +3,24 @@ package newMalli;
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.Actions;
 
 
 
 public class Tessssst {
-     public static void main(String[] args) {
+     public static void main(String[] args) throws InterruptedException {
         System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\selenuim\\RameshSoft\\Drivers\\chromedriver.exe");
     	WebDriver driver = new ChromeDriver();	
 		driver.get("https://www.google.com/");
 		driver.manage().window().maximize();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
-		    
-		 WebElement text = driver.findElement(By.name("q"));
-		 text.sendKeys("Telugu news");
-//		 text.click();
-		 text.submit();
-		 driver.findElement(By.xpath("//*[@id='rso']/div[2]/div/div[2]//div/div/a/h3")).click();
-		 
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
-		
+		driver.close();
+		   	
 		
 		
 	}
