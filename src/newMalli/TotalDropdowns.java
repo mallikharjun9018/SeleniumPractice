3f2aea2ddfcc4193472319d4e7021e12f8a684a8
mallikharjun9018 <a1.mallikharjunareddy@gmail.com>
2020-02-17 23:46:36 +0530

    First commit

---
+++ src/newMalli/TotalDropdowns.java
@@ -0,0 +1,44 @@
+package newMalli;
+
+import java.util.List;
+import java.util.ListIterator;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+public class TotalDropdowns {
+
+	public static void main(String[] args) {
+		System.setProperty("webdriver.chrome.driver", "C:\\Users\\User\\selenuim\\RameshSoft\\Drivers\\chromedriver.exe");
+		RemoteWebDriver driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		
+		driver.get("https://www.facebook.com");
+		List<WebElement> elements=driver.findElementsByXPath("//*");
+		int count=0;
+		int frame=0;
+		ListIterator ltr = elements.listIterator();
+		/*while (ltr.hasNext())
+		{
+		WebElement ele=(WebElement) ltr.next();
+		String tagName=ele.getTagName();
+		if(tagName.equalsIgnoreCase("select"))
+				count++;*/
+		
+		for(WebElement ele :elements)
+		{
+			if(ele.getTagName().equalsIgnoreCase("select"))
+		       count++;
+			else
+				if(ele.getTagName().equalsIgnoreCase("a"))
+					frame++;
+	}
+		System.out.println("No.of drodowns are : " +count);
+		System.out.println("no.of links  are : " +frame);
+	}
+	
+}
